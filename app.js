4d1b662038e996877870e4d787c2751d71c6e16c
RajKumar Gupta <Rajg@octallocal.com>
2026-08-06 10:21:58 +0530

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Demo Node App deployed using Argo CD 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
