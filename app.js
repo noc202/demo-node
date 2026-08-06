f2cb491f9ac82df152ae06a395436902837dff67
arpitk <arpit.khandelwal@octalsoftware.com>
2026-08-06 15:59:12 +0530

    test argocd image updater

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Demo Node App deployed using Argo CD  🚀");
+    res.send("Welcome to Demo Node App deployed using Argo CD Deployemnt  🚀");
 });
 
 app.get("/health", (req, res) => {
