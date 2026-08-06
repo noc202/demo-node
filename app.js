28a4e0d930270f4be7f60a807d8c7b865bd20a1b
noc202 <80098281+noc202@users.noreply.github.com>
2026-08-06 12:14:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Demo Node App deployed using Argo CD 🚀");
+    res.send("Welcome to Demo Node App deployed using Argo CD Deployment 🚀");
 });
 
 app.get("/health", (req, res) => {
